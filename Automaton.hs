f9622603f80dc2e60b2d16dc699ecb9f3c97c129
Pavel Bakhvalov <pavelkrot@gmail.com>
2019-03-20 22:13:26 +0300

    minimization + new errors

--- Automaton.hs
+++ Automaton.hs
@@ -3,17 +3,19 @@
 module Automaton where
 
 import           Data.Char
-import           Data.Maybe
 import           Data.List
+import qualified Data.Map                 as Map
+import           Data.Maybe
 import qualified Data.MultiMap            as MultiMap
 import qualified Data.Set                 as Set
-import qualified Data.Map                 as Map
 
 import           Combinators
 import           Control.Monad.State.Lazy
 
 type Set = Set.Set
+
 type Map = Map.Map
+
 type MultiMap = MultiMap.MultiMap
 
 data Automaton s q = Automaton
@@ -23,10 +25,13 @@ data Automaton s q = Automaton
   , termState :: Set q
   , epsilon   :: s
   , delta     :: MultiMap (q, s) (Maybe q)
-  } deriving (Show)
+  } deriving (Show, Eq)
 
 instance (Show k, Show v) => Show (MultiMap k v) where
   show = show . MultiMap.toList
+
+instance (Ord k, Ord v) => Eq (MultiMap k v) where
+  x == y = (sort $ MultiMap.toList x) == (sort $ MultiMap.toList y)
 
 -- Checks if the automaton is deterministic (only one transition for each state and each input symbol)
 isDFA :: Eq a => Automaton a b -> Bool
@@ -46,8 +51,8 @@ isComplete a =
   fromIntegral ((Set.size (sigma a)) * (Set.size (states a)))
 
 -- Checks if the automaton is minimal (only for DFAs: the number of states is minimal)
-isMinimal :: Automaton a b -> Bool
-isMinimal = undefined
+isMinimal :: Automaton String String -> Bool
+isMinimal a = (minimize a) == a
 
 transformToCompleteDeterministic ::
      (Ord a, Ord b) => Automaton a b -> Automaton a b
@@ -94,7 +99,8 @@ determinize a =
         Set.empty
         MultiMap.empty
       where
-        determinize'' :: Automaton String String
+        determinize'' ::
+             Automaton String String
           -> State (DeterminizeState String [String]) (Automaton String String)
         determinize'' a = do
           queue' <- gets queue
@@ -104,46 +110,265 @@ determinize a =
               trms <- gets newTerminals
               dlts <- gets newDeltas
               return $
-                 Automaton
-                   (sigma a)
-                   (Set.map (intercalate ",") sts)
-                   (initState a)
-                   (Set.map (intercalate ",") trms)
-                   (epsilon a)
-                   (MultiMap.map (\x -> case x of 
-                                          Just v -> Just (intercalate "," v) 
-                                          _ -> Nothing) $ (MultiMap.mapKeys (\(xs, s) -> ((intercalate "," xs), s)) dlts))
+                Automaton
+                  (sigma a)
+                  (Set.map (intercalate ",") sts)
+                  (initState a)
+                  (Set.map (intercalate ",") trms)
+                  (epsilon a)
+                  (MultiMap.map
+                     (\x ->
+                        case x of
+                          Just v -> Just (intercalate "," v)
+                          _      -> Nothing) $
+                   (MultiMap.mapKeys
+                      (\(xs, s) -> ((intercalate "," xs), s))
+                      dlts))
             else do
               let sts = sort $ Set.elemAt 0 queue'
-              modify (\s -> s {queue = Set.deleteAt 0 (queue s), newStates = Set.insert sts (newStates s)})
-
-              when (any (\v -> Set.member v (termState a)) sts) $ modify (\s -> s {newTerminals = Set.insert sts (newTerminals s)})
-
-              let outst = sort $ nub $ [end | ((begin, _), Just end) <- MultiMap.toList (delta a), elem begin sts]
-              let outc = nub $ [c | ((begin, c), _) <- MultiMap.toList (delta a), elem begin sts]
-
+              modify
+                (\s ->
+                   s
+                     { queue = Set.deleteAt 0 (queue s)
+                     , newStates = Set.insert sts (newStates s)
+                     })
+              when (any (\v -> Set.member v (termState a)) sts) $
+                modify
+                  (\s -> s {newTerminals = Set.insert sts (newTerminals s)})
+              let outst =
+                    sort $
+                    nub $
+                    [ end
+                    | ((begin, _), Just end) <- MultiMap.toList (delta a)
+                    , elem begin sts
+                    ]
+              let outc =
+                    nub $
+                    [ c
+                    | ((begin, c), _) <- MultiMap.toList (delta a)
+                    , elem begin sts
+                    ]
               newsts <- gets newStates
-              when (Set.notMember outst newsts && outst /= []) $ modify (\s -> s {queue = Set.insert outst (queue s) })
-
-              modify (\s -> s {newDeltas = foldr (\c m-> MultiMap.insert (sts, c) (Just outst) m) (newDeltas s) outc})
-
+              when (Set.notMember outst newsts && outst /= []) $
+                modify (\s -> s {queue = Set.insert outst (queue s)})
+              modify
+                (\s ->
+                   s
+                     { newDeltas =
+                         foldr
+                           (\c m -> MultiMap.insert (sts, c) (Just outst) m)
+                           (newDeltas s)
+                           outc
+                     })
               determinize'' a
 
-epsilonClosure:: Automaton String String -> Automaton String String
-epsilonClosure a = if notElem (epsilon a) (Prelude.fmap snd $ MultiMap.keys (delta a)) then a else
-  let epsilons = [(begin, end) | ((begin, c), Just end) <- MultiMap.toList (delta a), c == epsilon a] in
-  Automaton
-    (sigma a)
-    (foldr (\(_, end) s -> if notElem end [e | ((_, c), Just e) <- MultiMap.toList (delta a), c /= epsilon a] then Set.delete end s else s) (states a) epsilons)
-    (initState a)
-    (foldr (\(begin, end) s -> if Set.member end (termState a) then 
-                                  if notElem end [e | ((_, c), Just e) <- MultiMap.toList (delta a), c /= epsilon a] then Set.delete end $ Set.insert begin s else Set.insert begin s
-                               else s) (termState a) epsilons)
-    (epsilon a)
-    (foldr (\(begin, end) d -> foldr (\k m -> MultiMap.delete k m)
-      (foldr (\(k, v) m -> MultiMap.insert k v m) d [((begin, c), Just e) | ((b, c), Just e) <- MultiMap.toList (delta a), b == end])
-      [(b, c) | ((b, c), Just e) <- MultiMap.toList (delta a), c == epsilon a || notElem b [e | ((_, c), Just e) <- MultiMap.toList (delta a), c /= epsilon a]]
-    ) (delta a) epsilons)
+epsilonClosure :: Automaton String String -> Automaton String String
+epsilonClosure a =
+  if notElem (epsilon a) (Prelude.fmap snd $ MultiMap.keys (delta a))
+    then a
+    else let epsilons =
+               [ (begin, end)
+               | ((begin, c), Just end) <- MultiMap.toList (delta a)
+               , c == epsilon a
+               ]
+          in Automaton
+               (sigma a)
+               (foldr
+                  (\(_, end) s ->
+                     if notElem
+                          end
+                          [ e
+                          | ((_, c), Just e) <- MultiMap.toList (delta a)
+                          , c /= epsilon a
+                          ]
+                       then Set.delete end s
+                       else s)
+                  (states a)
+                  epsilons)
+               (initState a)
+               (foldr
+                  (\(begin, end) s ->
+                     if Set.member end (termState a)
+                       then if notElem
+                                 end
+                                 [ e
+                                 | ((_, c), Just e) <- MultiMap.toList (delta a)
+                                 , c /= epsilon a
+                                 ]
+                              then Set.delete end $ Set.insert begin s
+                              else Set.insert begin s
+                       else s)
+                  (termState a)
+                  epsilons)
+               (epsilon a)
+               (foldr
+                  (\(begin, end) d ->
+                     foldr
+                       (\k m -> MultiMap.delete k m)
+                       (foldr
+                          (\(k, v) m -> MultiMap.insert k v m)
+                          d
+                          [ ((begin, c), Just e)
+                          | ((b, c), Just e) <- MultiMap.toList (delta a)
+                          , b == end
+                          ])
+                       [ (b, c)
+                       | ((b, c), Just e) <- MultiMap.toList (delta a)
+                       , c == epsilon a ||
+                           notElem
+                             b
+                             [ e
+                             | ((_, c), Just e) <- MultiMap.toList (delta a)
+                             , c /= epsilon a
+                             ]
+                       ])
+                  (delta a)
+                  epsilons)
+
+dfs :: Eq q => q -> MultiMap (q, s) (Maybe q) -> [q]
+dfs s m = s : dfs' s []
+  where
+    dfs' s sts =
+      nub $
+      foldr
+        (\s' sts' -> dfs' s' (s' : sts'))
+        sts
+        [ e
+        | ((b, _), Just e) <- MultiMap.toList m
+        , b == s && notElem e sts && e /= s
+        ]
+
+removeUnreachable :: Automaton String String -> Automaton String String
+removeUnreachable a =
+  let reachable = dfs (initState a) (delta a)
+   in Automaton
+        (sigma a)
+        (foldr
+           (\st sts ->
+              if elem st reachable
+                then sts
+                else Set.delete st sts)
+           (states a)
+           (Set.toList $ states a))
+        (initState a)
+        (foldr
+           (\st sts ->
+              if elem st reachable
+                then sts
+                else Set.delete st sts)
+           (termState a)
+           (Set.toList $ termState a))
+        (epsilon a)
+        (foldr
+           (\k m -> MultiMap.delete k m)
+           (delta a)
+           [ (b, c)
+           | ((b, c), Just e) <- MultiMap.toList (delta a)
+           , notElem b reachable || notElem e reachable
+           ])
+
+data MinimizeState q = MinimizeState
+  { queue' :: Set (q, q)
+  , table  :: Set (q, q)
+  }
+
+minimize :: Automaton String String -> Automaton String String
+minimize a =
+  minimize' $
+  removeUnreachable
+    (if not $ isDFA a
+       then error "Non determenistic"
+       else if isComplete a
+              then a
+              else transformToCompleteDeterministic a)
+  where
+    reverseMapping =
+      MultiMap.fromList
+        [ ((end, sigma), begin)
+        | ((begin, sigma), Just end) <- MultiMap.toList (delta a)
+        ]
+    minimize' a =
+      evalState (minimize'' a) $
+      MinimizeState
+        (Set.fromList
+           [ (x, y)
+           | x <- (Set.toList $ states a)
+           , y <- (Set.toList $ termState a)
+           , x /= y
+           , not (elem x (termState a) && elem y (termState a))
+           ])
+        (Set.fromList
+           [ (x, y)
+           | x <- (Set.toList $ states a)
+           , y <- (Set.toList $ termState a)
+           , x /= y
+           , not (elem x (termState a) && elem y (termState a))
+           ])
+      where
+        minimize'' ::
+             Automaton String String
+          -> State (MinimizeState String) (Automaton String String)
+        minimize'' a = do
+          queue'' <- gets queue'
+          if null queue''
+            then do
+              table' <- gets table
+              let sts =
+                    [ (s, ss)
+                    | s <- Set.toList $ states a
+                    , ss <- [sort [y | (x, y) <- Set.toList table', x == s]]
+                    ]
+              let groups = groupBy (\st1 st2 -> snd st1 == snd st2) sts
+              let result = map (\ss -> map fst ss) groups
+              return $
+                Automaton
+                  (sigma a)
+                  (Set.fromList $ map (intercalate ",") result)
+                  (intercalate "," $
+                   head [x | x <- result, elem (initState a) x])
+                  (Set.fromList $
+                   map
+                     (intercalate ",")
+                     [x | x <- result, any (\x' -> elem x' (termState a)) x])
+                  (epsilon a)
+                  (MultiMap.fromList
+                     [ (((intercalate "," x), y), Just (intercalate "," z))
+                     | x <- result
+                     , z <- result
+                     , y <- Set.toList $ sigma a
+                     , any
+                         (\x' ->
+                            any
+                              (\x'' ->
+                                 case x'' of
+                                   Just x''' -> elem x''' z
+                                   Nothing   -> False) $
+                            MultiMap.lookup (x', y) (delta a))
+                         x
+                     ])
+            else do
+              let (x, y) = Set.elemAt 0 queue''
+              table' <- gets table
+              let newPairs =
+                    [ (f, s)
+                    | (x', y') <-
+                        [((x, s'), (y, s')) | s' <- Set.toList $ sigma a]
+                    , f <- MultiMap.lookup x' reverseMapping
+                    , s <- MultiMap.lookup y' reverseMapping
+                    , f < s
+                    , not $ Set.member (f, s) table'
+                    ]
+              modify
+                (\s ->
+                   s
+                     { queue' =
+                         Set.union
+                           (Set.deleteAt 0 (queue' s))
+                           (Set.fromList newPairs)
+                     })
+              modify
+                (\s -> s {table = Set.union (table s) (Set.fromList newPairs)})
+              minimize'' a
 
 parserAutomaton :: Parser Char String (Automaton [Char] [Char])
 parserAutomaton = do
@@ -196,9 +421,10 @@ parserAutomaton = do
           then if elem end states
                  then if elem sigma ("\\epsilon" : alphabet)
                         then return ((begin, sigma), Just end)
-                        else Combinators.fail "Sigma is not in the alphabet\n"
-                 else Combinators.fail "End state is not in the states\n"
-          else Combinators.fail "Begin state is not in the states\n"
+                        else Combinators.fail $
+                             "Sigma '" ++ sigma ++ "' is not in the alphabet"
+                 else Combinators.fail "End state is not in the states"
+          else Combinators.fail "Begin state is not in the states"
   delts <- parseList triplets (char ',') (char '<') (char '>') (>= 0)
   skipSpaces
   stringEof
@@ -219,7 +445,7 @@ parserAutomaton = do
 -- * Any of the terminal states is not a state
 -- * Delta function is defined on not-a-state or not-a-symbol-from-sigma
 -- Pick appropriate types for s and q
-parseAutomaton :: String -> Either String (Automaton String String)
+parseAutomaton :: String -> Either [String] (Automaton String String)
 parseAutomaton input =
   case runParser parserAutomaton (stream input) of
     Left e       -> Left e
@@ -286,8 +512,28 @@ test_IS_COMPLETE_TRUE_MULTI =
 testTransformToCompleteDeterministic =
   isComplete $ transformToCompleteDeterministic $ fromEither test3
 
-testNFAtoDFA = parseAutomaton "<a,b,c> <1,2,3> <1> <3> <(1,c,2),(1,c,3),   (2,a,3),(2,b,3),(2,c,3),    (3,a,1),(3,b,1),(3,c,1)>"
+testNFAtoDFA =
+  parseAutomaton
+    "<a,b,c> <1,2,3> <1> <3> <(1,c,2),(1,c,3),   (2,a,3),(2,b,3),(2,c,3),    (3,a,1),(3,b,1),(3,c,1)>"
 
-testEpsilonClosure1 = parseAutomaton "<a,b,c> <1,2,3> <1> <3> <(1,\\epsilon,2),(2,a,3)>"
-testEpsilonClosure2 = parseAutomaton "<a,b,c> <1,2,3,4> <1> <3> <(1,\\epsilon,2),(2,a,3),(4,b,2)>"
+testEpsilonClosure1 =
+  parseAutomaton "<a,b,c> <1,2,3> <1> <3> <(1,\\epsilon,2),(2,a,3)>"
+
+testEpsilonClosure2 =
+  parseAutomaton "<a,b,c> <1,2,3,4> <1> <3> <(1,\\epsilon,2),(2,a,3),(4,b,2)>"
+
 testEpsilonClosure3 = parseAutomaton "<a,b,c> <1,2> <1> <2> <(1,\\epsilon,2)>"
+
+testMinimization =
+  minimize $
+  fromEither $
+  parseAutomaton
+    "<0,1> <A,B,C,D,E,F,G> <A> <F,G> <(A,0,C),(A,1,B),(B,1,A),(B,0,C),(C,0,D),(C,1,D),(D,0,E),(D,1,F),(E,0,F),(E,1,G),(F,0,F),(F,1,F),(G,0,G),(G,1,F)>"
+
+testIsMinimalTrue = isMinimal $ fromEither $ test3
+
+testIsMinimalFalse =
+  isMinimal $
+  fromEither $
+  parseAutomaton
+    "<0,1> <A,B,C,D,E,F,G> <A> <F,G> <(A,0,C),(A,1,B),(B,1,A),(B,0,C),(C,0,D),(C,1,D),(D,0,E),(D,1,F),(E,0,F),(E,1,G),(F,0,F),(F,1,F),(G,0,G),(G,1,F)>"
--- Combinators.hs
+++ Combinators.hs
@@ -10,24 +10,25 @@ import qualified Prelude
 
 -- Parsing result is either an error message or some payload and a suffix of the input which is yet to be parsed
 newtype Parser token err ok = Parser
-  { runParser :: Stream token -> Either err (Stream token, ok)
+  { runParser :: Stream token err -> Either [err] (Stream token err, ok)
   }
 
-data Stream token = Stream
+data Stream token err = Stream
   { tokens :: [token]
   , len    :: Int
   , still  :: Int
+  , errors :: [err]
   } deriving (Show)
 
-stream :: [token] -> Stream token
-stream tokens = Stream tokens len len
+stream :: [token] -> Stream token err
+stream tokens = Stream tokens len len []
   where
     len = length tokens
 
-streamS :: Stream token -> (Maybe token, Stream token)
-streamS (Stream [] len _) = (Nothing, Stream [] len 0)
-streamS (Stream (token:tokens) len still) =
-  (Just token, Stream tokens len (still - 1))
+streamS :: Stream token err -> (Maybe token, Stream token err)
+streamS (Stream [] len _ errs) = (Nothing, Stream [] len 0 errs)
+streamS (Stream (token:tokens) len still errs) =
+  (Just token, Stream tokens len (still - 1) errs)
 
 -- streamS :: token -> Stream token -> Stream token
 -- streamS token (Stream tokens all curr) = Stream (token : tokens) all (curr + 1)
@@ -68,18 +69,20 @@ success ok = Parser $ \s -> Right (s, ok)
 
 -- Parser which fails no mater the input
 fail :: err -> Parser str err ok
-fail err = Parser $ \_ -> Left err
+fail err = Parser $ \(Stream _ _ _ errs) -> Left (err : errs)
 
+-- ((show $ ln str - st str) ++ " pos, " ++ err)
 -- Biased choice: if the first parser succeedes, the second is never run
 (<|>) ::
      Monoid err => Parser str err ok -> Parser str err ok -> Parser str err ok
 p <|> q =
   Parser $ \s ->
     case runParser p s of
-      Left err ->
+      Left errs ->
         case runParser q s of
-          Left err' -> Left $ err <> err'
-          x         -> x
+          Left errs' -> Left $ errs <> errs'
+          Right (Stream ts ln stl s'_errs, r) ->
+            Right (Stream ts ln stl (errs <> s'_errs), r)
       x -> x
 
 -- Monadic sequence combinator
@@ -114,10 +117,10 @@ some :: Parser str err a -> Parser str err [a]
 some p =
   Parser $ \s ->
     case runParser p s of
-      Right (s', r) ->
+      Right (s'@(Stream ts ln st _), r) ->
         case (runParser $ some p) s' of
           Right (s'', rs) -> Right (s'', r : rs)
-          _               -> Right (s', [r])
+          Left new_errs   -> Right (Stream ts ln st new_errs, [r])
       Left e -> Left e
 
 -- Applies a parser zero or more times
@@ -125,37 +128,41 @@ many :: Parser str err a -> Parser str err [a]
 many p =
   Parser $ \s ->
     case runParser p s of
-      Right (s', r) ->
+      Right (s'@(Stream ts ln st _), r) ->
         case (runParser $ many p) s' of
           Right (s'', rs) -> Right (s'', r : rs)
-          _               -> Right (s', [r])
-      _ -> Right (s, [])
+          Left errs       -> Right (Stream ts ln st errs, [r])
+      Left err -> Right (s, [])
 
 -- Checks if the first element of the input is the given token
 token :: (Eq token, Show token) => token -> Parser token String token
 token t =
-  Parser $ \s ->
+  Parser $ \s@(Stream _ _ _ errs) ->
     case s of
       (streamS -> (Just t', str))
         | t == t' -> Right (str, t)
         | otherwise ->
           Left $
-          (show $ len str - still str) ++
-          " pos, " ++ (show t') ++ " is not a token!\n"
-      _ -> Left $ "Empty input!\n"
+          [ (show $ len str - still str) ++
+            " pos, " ++ (show t') ++ " is not a token!"
+          ] <>
+          errs
+      _ -> Left $ ["Empty input!"] <> errs
 
 satisfy :: (Show token) => (token -> Bool) -> Parser token String token
 satisfy f =
-  Parser $ \s ->
+  Parser $ \s@(Stream _ _ _ errs) ->
     case s of
       (streamS -> (Just t, str))
         | f t -> Right (str, t)
         | otherwise ->
           Left $
-          (show $ len str - still str) ++
-          " pos, " ++ (show t) ++ " is not a token!\n"
+          [ (show $ len str - still str) ++
+            " pos, " ++ (show t) ++ " is not a token!"
+          ] <>
+          errs
       (streamS -> (Nothing, str)) ->
-        Left $ (show $ len str - still str) ++ " pos, Empty input!\n"
+        Left $ [(show $ len str - still str) ++ " pos, Empty input!"] <> errs
 
 -- Checks if the first character of the string is the one given
 char :: Char -> Parser Char String Char
@@ -163,20 +170,20 @@ char = token
 
 stringEof :: Parser Char String Char
 stringEof =
-  Parser $ \s ->
+  Parser $ \s@(Stream _ _ _ errs) ->
     case s of
       (streamS -> (Nothing, str)) -> Right (str, '\0')
       (streamS -> (_, str)) ->
-        Left $ (show $ len str - still str) ++ " pos, Not the end of string!\n"
+        Left $
+        [(show $ len str - still str) ++ " pos, Not the end of string!"] <> errs
 
-zeroOne :: Eq token => token -> Parser token err token
-zeroOne t =
-  Parser $ \s ->
-    case s of
-      (streamS -> (Just t', str))
-        | t == t' -> Right (str, t)
-      _ -> Right (s, t)
-
+-- zeroOne :: Eq token => token -> Parser token err token
+-- zeroOne t =
+--   Parser $ \s ->
+--     case s of
+--       (streamS -> (Just t', str))
+--         | t == t' -> Right (str, t)
+--       _ -> Right (s, t)
 try :: Monoid err => Parser Char err a -> Parser Char err (Maybe a)
 try p = fmap Just p <|> success Nothing
 
@@ -184,26 +191,30 @@ keywords_satisfy :: [String] -> (Char -> Bool) -> Parser Char String String
 keywords_satisfy kws p = Parser $ \s -> next s trie []
   where
     trie = foldr insert (Trie [] False) kws
-    next input@(streamS -> (t, str)) (Trie f_ing s) ok =
+    next input@(streamS -> (t, str@(Stream _ _ _ errs))) (Trie f_ing s) ok =
       case t of
         Nothing ->
           if s
             then Right (input, ok)
-            else Left "Empty trie!\n"
+            else Left $ ["Empty trie!"] <> errs
         Just t
           | p t ->
             if s
               then Right (input, ok)
               else Left $
-                   (show $ len str - still str) ++
-                   " pos, " ++ ok ++ " is not a token!\n"
+                   [ (show $ len str - still str) ++
+                     " pos, " ++ ok ++ " is not a token!"
+                   ] <>
+                   errs
           | otherwise ->
             case lookup t f_ing of
               Just t' -> next str t' (ok ++ [t])
               _ ->
                 Left $
-                (show $ len str - still str) ++
-                " pos, " ++ ok ++ [t] ++ " is not a token!\n"
+                [ (show $ len str - still str) ++
+                  " pos, " ++ ok ++ [t] ++ " is not a token!"
+                ] <>
+                errs
 
 parseList ::
      Parser Char String el
@@ -224,10 +235,14 @@ parseList el delim lbr rbr p = do
     Nothing -> do
       if p $ 0
         then return []
-        else fail "Empty input!\n"
+        else fail "Empty input!"
     Just v -> do
       if p $ length elms + 1
         then return (v : elms)
-        else fail "Incorrect number of elements\n"
+        else fail "Incorrect number of elements"
 
-test_position = (runParser $ (keywords_satisfy ["abc"] (\c -> c == ' ')) <|> (keywords_satisfy ["abcde"] (\c -> c == ' ')) ) (stream "abcdef")
+test_position =
+  (runParser $
+   (keywords_satisfy ["abc"] (\c -> c == ' ')) <|>
+   (keywords_satisfy ["abcde"] (\c -> c == ' ')))
+    (stream "abcdef")
