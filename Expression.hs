f3ae7308ca722e570792add015a95fa107f38a27
Pavel Bakhvalov <pavelkrot@gmail.com>
2019-04-10 22:42:47 +0300

    HW08

--- Expression.hs
+++ Expression.hs
@@ -21,17 +21,45 @@ data Operator = Pow
 data EAst a = BinOp Operator (EAst a) (EAst a)
             | Primary a
 
+pnum = do
+  f <- spaces *> (digit <|> char '0')
+
+  case f of
+    '0' -> return $ Primary 0
+    _ -> do
+      s <- many $ (digit <|> char '0')
+      return $ Primary (read (f : s) :: Integer)  
+
+calcPnum = do
+  f <- spaces *> (digit <|> char '0')
+
+  case f of
+    '0' -> return 0
+    _ -> do
+      s <- many $ (digit <|> char '0')
+      return (read (f : s) :: Integer)  
+            
+
+parserUntilEof :: Parser Char String a -> Parser Char String a
+parserUntilEof p = do
+  r <- p
+  spaces
+  eof
+  return r
+
 -- Change the signature if necessary
 -- Constructs AST for the input expression
 parseExpression :: String -> Either String (EAst Integer)
-parseExpression input = 
-  runParserUntilEof (expression undefined undefined) input
+parseExpression input = case runParser (parserUntilEof $ expression specification pnum) (stream input) of
+  Left e -> Left $ concat e
+  Right (_, a) -> Right a
 
 -- Change the signature if necessary
 -- Calculates the value of the input expression
 executeExpression :: String -> Either String Integer
-executeExpression input = 
-  runParserUntilEof (expression undefined undefined) input
+executeExpression input = case runParser (parserUntilEof $ expression calcSpecification calcPnum) (stream input) of
+  Left e -> Left $ concat e
+  Right (_, a) -> Right a
 
 instance Show Operator where
   show Pow   = "^"
@@ -48,6 +76,31 @@ instance Show Operator where
   show Conj  = "&&"
   show Disj  = "||"
 
+specification =
+  [
+    (LAssoc, [ (string "||", BinOp Disj) ]),
+    (RAssoc, [ (string "&&", BinOp Conj) ]),
+    (NAssoc, [ (string "==", BinOp Eq), (string "/=", BinOp Neq), (string "<=", BinOp Le), (string ">=", BinOp Ge), (string "<", BinOp Lt), (string ">", BinOp Gt) ]),
+    (LAssoc, [ (string "+", BinOp Sum), (string "-", BinOp Minus) ]),
+    (LAssoc, [ (string "*", BinOp Mul), (string "/", BinOp Div) ]),
+    (RAssoc, [ (string "^", BinOp Pow) ])
+  ]
+
+calcSpecification =
+  [
+    (LAssoc, [ (string "||", (\f s -> if f /= 0 || s /= 0 then 1 else 0)) ]),
+    (RAssoc, [ (string "&&", (\f s -> if f /= 0 && s /= 0 then 1 else 0)) ]),
+    (NAssoc, [ (string "==", (\f s -> if f == s then 1 else 0)),
+               (string "/=", (\f s -> if f /= s then 1 else 0)),
+               (string "<=", (\f s -> if f <= s then 1 else 0)),
+               (string ">=", (\f s -> if f >= s then 1 else 0)),
+               (string "<",  (\f s -> if f < s then 1 else 0)),
+               (string ">",  (\f s -> if f > s then 1 else 0)) ]),
+    (LAssoc, [ (string "+", (+)), (string "-", (-)) ]),
+    (LAssoc, [ (string "*", (*)), (string "/", div) ]),
+    (RAssoc, [ (string "^", (^)) ])
+  ]
+
 instance Show a => Show (EAst a) where
   show = show' 0
     where
@@ -57,15 +110,3 @@ instance Show a => Show (EAst a) where
                   BinOp op l r -> printf "%s\n%s\n%s" (show op) (show' (ident n) l) (show' (ident n) r)
                   Primary x -> show x)
       ident = (+1)
-
-{-
-show (BinOp Conj (BinOp Pow (Primary 1) (BinOp Sum (Primary 2) (Primary 3))) (Primary 4))
-
-&&
-|_^
-| |_1
-| |_+
-| | |_2
-| | |_3
-|_4
--}
